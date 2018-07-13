1305adcef82a09023cd3d883d913deb3d098d5da
Nicolas <nicolas.demay@anaxago.com>
2018-07-13 15:58:36 +0200

    Handle login form

--- src/Anaxago/CoreBundle/Controller/DefaultController.php
+++ src/Anaxago/CoreBundle/Controller/DefaultController.php
@@ -1,14 +1,22 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace Anaxago\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class DefaultController
+ *
+ * @package Anaxago\CoreBundle\Controller
+ */
 class DefaultController extends Controller
 {
-    public function indexAction()
+    /**
+     * @return Response
+     */
+    public function indexAction(): Response
     {
-       return $this->render('@AnaxagoCore/Default/index.html.twig');
+        return $this->render('@AnaxagoCore/Default/index.html.twig');
     }
 }
